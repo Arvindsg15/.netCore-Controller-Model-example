f54a6490265ccc3e5f1994b8a490943dc7d53bac
Arvind Gonaki <arvindsg15@gmail.com>
2020-07-30 00:19:09 +0530

    Add project files.

---
+++ WebApplication1/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Repository;
+
+namespace WebApplication1.Controllers
+{
+    public class BookController : Controller
+    { 
+        private readonly BookRepository _bookrepository = null;
+        public BookController()
+        {
+            _bookrepository = new BookRepository();
+        }
+
+        public List<BookModel> GetBooks()
+        {
+            return _bookrepository.GetAllBooks();
+        }
+
+        public BookModel GetById(int id)
+        {
+            return _bookrepository.GetBookById(id);
+        }
+
+        public List<BookModel> Search(string title,string author)
+        {
+            return _bookrepository.SearchBook(title, author);
+        }
+    }
+}
---
+++ WebApplication1/Repository/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Repository
+{
+    public class BookRepository
+    {
+
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(string title ,string authorname)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) && x.Author.Contains(authorname)).ToList();
+        }
+
+        private List<BookModel> DataSource ()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=101,Title="MVC",Author="Nitish"},
+                new BookModel(){Id=102,Title="Java",Author="Abhi"},
+                new BookModel(){Id=103,Title="C#",Author="RN Reddy"},
+
+            };
+        }
+    }
+}
